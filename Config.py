31e3bd14e8171716155793d37029069f3bf1a9f1
DyingEngi <eryuentoh@gmail.com>
2025-03-17 17:29:38 +0000

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,4 +1,4 @@
 # API Key & Robot Configuration
-GEMINI_API_KEY = "AIzaSyClHgQyP1QNTNjWM0edwWlF0NwJYpMJ0cI"
-ROBOT_IP = "192.168.56.101"  # Change to your UR10e's IP
-PORT = 50002  # Custom URScript command port configured on the robot
+GEMINI_API_KEY = "enter here"
+ROBOT_IP = "enter here"  # Change to your UR10e's IP
+PORT = "enter here"  # Custom URScript command port configured on the robot
